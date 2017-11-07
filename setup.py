65097a10533995c106a11975218f571e5abf199d
Josh Paul <josh.paul@rackspace.com>
2017-11-07 12:17:00 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     version='0.0.1',
     description=('Simple REST api to create, validate, and fingerprint ssh keys'),
     long_description=desc,
-    url='https://github.com/josh-paul/check_servers',
+    url='https://github.com/josh-paul/ssh_key_rest',
     author='Josh Paul',
     author_email='trevalen@me.com',
     license='Apache v2',
@@ -23,9 +23,14 @@ setup(
         'Programming Language :: Python :: 3.6',
     ],
 
-    keywords='ssh key  rest ssh-key ssh-key-rest',
+    keywords='ssh key rest ssh-key ssh-key-rest',
     packages=find_packages(exclude=['contrib', 'docs', 'test*']),
-    install_requires=['futures', 'requests', 'tabulate'],
+    install_requires=[
+        'cryptography>=1.4.0',
+        'falcon>=1.3.0',
+        'falcon-lambda>=0.1.2',
+        'jsonschema'
+    ],
     extras_require={},
     package_data={},
     data_files=[],
