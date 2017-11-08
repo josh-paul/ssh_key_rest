7bd917cab8f6bfb3682cd1532bb567197997f11f
Josh Paul <josh.paul@rackspace.com>
2017-11-08 08:19:56 -0800

    Added some doc strings to Falcon and Lambda bits

--- handler.py
+++ handler.py
@@ -7,10 +7,15 @@ from ssh_key.app import Service
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+# Init the Falcon API
 api = Service()
 
 
 def lambda_handler(event, context):
+    '''
+    Wrap the Falcon API with a WSGI handler built to handle traffic from API-Gateway to Lambda.
+    This is the entrypoint to be used in the Lambda conofiguration. handler.lambda_handler
+    '''
     logger.debug(event)
     resp = wsgi.adapter(api, event, context)
     logger.debug(resp)
--- ssh_key/app.py
+++ ssh_key/app.py
@@ -4,6 +4,9 @@ from ssh_key.resources import KeyResource
 
 
 class Service(falcon.API):
+    '''
+    Class that wraps up the full init of the API.
+    '''
     def __init__(self):
         super().__init__()
 
@@ -11,7 +14,13 @@ class Service(falcon.API):
         self.setup_routes()
 
     def create_resources(self):
+        '''
+        Pull in all endpoints to class vars.
+        '''
         self.key_res = KeyResource()
 
     def setup_routes(self):
+        '''
+        Map URI routes to the class vars for endpoints.
+        '''
         self.add_route('/ssh/key', self.key_res)
